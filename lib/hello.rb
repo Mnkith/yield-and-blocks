b3295f5499b0ab91a30239680bfc778ab4be1fc1
Mnkith <munkithabid@gmail.com>
2020-10-23 19:10:11 -0500

    commit tree d4ebfcdad21702ef0d86eff3b9a1e2b127d92c95

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,11 @@
-def hello_t
-
+def hello_t(names)
+  if block_given? 
+    i = 0
+    until i == names.size
+      yield(names[i])
+      i += 1
+    end
+  else puts "Hey! No block was given!"
+  end
+  names
 end
-
-# call your method here!
-
